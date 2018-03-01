a0dd27973af43ae9d6633a8a7bcfff080d690c48
IanBoyanZhang <SillyKnight1986@gmail.com>
2018-03-01 10:57:41 -0800

    init commit of project files

---
+++ polyspace/Module_1/BF_Result/.settings/launchingCommand.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"/Applications/MATLAB_R2017b.app/polyspace/bin/polyspace-bug-finder-nodesktop" -lang CPP -options-file "/Users/idefixthegrand/Downloads/psprj_for_udacity/polyspace/Module_1/BF_Result/.settings/options_command.txt" -results-dir "/Users/idefixthegrand/Downloads/psprj_for_udacity/polyspace/Module_1/BF_Result" $*
